60f639293b45e9c1bb655a65680d54d3319919d5
Meng Jia <jiameng_73@hotmail.com>
2018-01-28 11:50:10 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import 'vue-hi-there';
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          <demo-basic></demo-basic>
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
