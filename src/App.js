87c98c0d2933bc64bf835aa6e61aee7206a07fc7
Meng Jia <meng.jia3@ubisoft.com>
2018-01-29 10:34:21 -0500

    update react app to consume two different vue components and one is nested

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import 'vue-hi-there';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import 'vue-hi-there'
 class App extends Component {
-  render() {
+  render () {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className='App'>
+        <header className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h1 className='App-title'>Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          <demo-basic></demo-basic>
+        <p className='App-intro'>
+          <span> First Vue Component </span>
+          <demo-basic>
+            <second-component prop1='1' prop2='string' prop3='true' />
+          </demo-basic>
+
+          <span> Second Vue Component </span>
+          <second-component prop1='1' prop2='Boolean' prop3='false' />
         </p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
